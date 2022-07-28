773f9a401444b4578f26c17ff7e0222ebc60a75f
NataliaZanegina <nataliazanegina@yandex.ru>
2022-07-28 20:56:36 +0300

    method ArrayOfElementsUpTo3Char added

--- Program.cs
+++ Program.cs
@@ -12,3 +12,20 @@ void Fill(string[] array)
             array[i] = Console.ReadLine();
     }
 }
+
+string[] ArrayOfElementsUpTo3Char(string[] array, int count)
+{
+    string[] newArray = new string[count];
+    int index = 0;
+    int size = array.Length;
+    for (int i = 0; i < size; i++)
+    {
+        int length = array[i].Length;
+        if (length <= 3)
+        {
+            newArray[index] = array[i];
+            index++;
+        }
+    }
+    return newArray;
+}
