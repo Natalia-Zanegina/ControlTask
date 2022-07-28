89a80a4d0783cdca71ecd2973450dcd3f60deead
NataliaZanegina <nataliazanegina@yandex.ru>
2022-07-28 20:47:22 +0300

    method Fill added

--- Program.cs
+++ Program.cs
@@ -2,3 +2,13 @@
 {
     return new string[size];
 }
+
+void Fill(string[] array)
+{
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+    {
+            Console.Write($"Input {i + 1} string: ");
+            array[i] = Console.ReadLine();
+    }
+}
